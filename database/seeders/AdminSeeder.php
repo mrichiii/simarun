da58f5a2e0c186b9836507252918b69a6d79fe7d
Muhammad Richie Hadiansah <mrichieh123@gmail.com>
2025-12-22 21:23:02 +0700

    buat seeder akun admin default

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::firstOrCreate(
+            ['email' => 'admin@uinsu.ac.id'],
+            [
+                'name' => 'Admin FST',
+                'password' => bcrypt('Admin@123'),
+                'role' => 'admin',
+            ]
+        );
+    }
+}
+
